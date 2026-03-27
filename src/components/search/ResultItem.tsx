91770a3aa7ea20838b8f9b5eaff1c2d780a0ea1c
Elidon Morina <83674499+3mper0r@users.noreply.github.com>
2026-03-27 22:05:11 +0100

    style: minor changes

--- src/components/search/ResultItem.tsx
+++ src/components/search/ResultItem.tsx
@@ -1,6 +1,6 @@
 export default function ResultItem({ item }: any) {
   return (
-    <div className="bg-white/80 h-[140px] backdrop-blur border border-gray-200 p-4 rounded-2xl shadow-sm space-y-4 dark:bg-gray-800/70 dark:border-gray-700">
+    <div className="bg-white/80 h-[140px] backdrop-blur border border-gray-200 p-4 rounded-2xl shadow-sm space-y-4 hover:bg-gray-100 hover:shadow-lg transition hover:cursor-pointer dark:bg-gray-800/70 dark:border-gray-700">
       <div className="flex justify-between items-start">
         <div className="text-xs text-gray-400 dark:text-gray-200">
           #{item.bildnummer}
